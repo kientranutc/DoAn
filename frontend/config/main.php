ed082d9d873231d84519429356d3409944bca2cd
kientranutc <kienkienutc95@gmail.com>
2016-12-28 13:28:25 +0700

    sua phanquyen

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -41,7 +41,8 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-           
+            
+
 
             ],
         ],
--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -51,7 +51,7 @@ class ContactForm extends Model
     public function sendEmail($email)
     {
         return Yii::$app->mailer->compose()
-            ->setTo($email)
+            ->setTo($emYiail)
             ->setFrom([$this->email => $this->name])
             ->setSubject($this->subject)
             ->setTextBody($this->body)
